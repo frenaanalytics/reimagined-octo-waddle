16e7d2e40a9a19ddff9a36a245f44dee459badfd
Anand Mahajan <aoracle@gmail.com>
2019-07-06 10:52:27 -0400

    repeat with 4 times with a for loop

--- draw.py
+++ draw.py
@@ -1,5 +1,13 @@
+# import turtle
+# painter = turtle.Turtle()
+# painter.forward(100)
+# painter.left(90)
+# turtle.done()
+#New Code
+
 import turtle
 painter = turtle.Turtle()
-painter.forward(100)
-painter.left(90)
+for _ in range(4):
+    painter.forward(100)
+    painter.left(90)
 turtle.done()
