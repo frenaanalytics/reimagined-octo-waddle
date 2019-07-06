faf3ab71e74198e934cf968c8fb2fd2ca2afc5b9
Anand Mahajan <aoracle@gmail.com>
2019-07-06 11:21:28 -0400

    Adding dotted circle

--- draw.py
+++ draw.py
@@ -5,17 +5,9 @@
 # turtle.done()
 #New Code
 
-# import turtle
-# painter = turtle.Turtle()
-# for _ in range(4):
-#     painter.forward(100)
-#     painter.left(90)
-# turtle.done()
-
 import turtle
 painter = turtle.Turtle()
-painter.pensize(20)
-for _ in range(18):
+for _ in range(4):
     painter.forward(100)
-    painter.left(100)
+    painter.left(90)
 turtle.done()
